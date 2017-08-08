da9204682fc18a234c6f04583d2e30c65e9d03d9
mygit <github@git.com>
2017-08-08 11:37:59 +0800

    ccc

---
+++ src/com/young/nativessh/NativeSSH.java
@@ -0,0 +1,8 @@
+package com.young.nativessh;
+
+public class NativeSSH {
+    public static void main(String[] args) {
+        System.out.println("native ssh");
+        System.out.println("native ssh");
+    }
+}
